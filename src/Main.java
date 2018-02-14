6ef2c9d41c59d460fd98792dfa2b6cd22f563531
Chad Buehrle <chad.buehrle@gmail.com>
2018-02-13 21:44:40 -0600

    sort largest to smallest

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         int[] arr = {2, 5, 1, 3, 15, 7, 11, 8, 1, 9, 23, 17};
         insertion(arr);
         System.out.println(Arrays.toString(arr));
+        insertionDesc(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void insertion(int[] array){
@@ -16,6 +18,18 @@ public class Main {
                 i = i - 1;
             }
             array[i + 1] = key;
+        }
+    }
+
+    public static void insertionDesc(int[] array){
+        for (int j = 1; j < array.length; j++) {
+           int key = array[j];
+           int i = j - 1;
+           while (i >= 0 && array[i] < key) {
+               array[i + 1] = array[i];
+               i = i - 1;
+           }
+           array[i + 1] = key;
         }
     }
 
